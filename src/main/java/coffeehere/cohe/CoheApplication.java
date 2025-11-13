c194c9874a1cc40f9eca4b6dbd6206fc0a52f4aa
Park Jae Seong <timer973@naver.com>
2025-11-13 16:12:05 +0900

    project [cohe] : git first init

---
+++ src/main/java/coffeehere/cohe/CoheApplication.java
@@ -0,0 +1,13 @@
+package coffeehere.cohe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coffeehere/cohe/CoheApplicationTests.java
@@ -0,0 +1,13 @@
+package coffeehere.cohe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
